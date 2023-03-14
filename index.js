d053dbe642bc84eaf48fba40a2bd3762c2388e54
Fuad <fuadabdaz99@gmail.com>
2023-03-14 11:41:10 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+ if (ride<=400){
+  return "This one is on me!";
+ }else if (ride>2000){
+  return 'I will gladly take your thirty bucks.'
+ }else if (ride>2500){
+  return 'No can do.'
+ }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if(city==='NYC'){
+    return 'Ok, sounds good.'
+  }else{ "'No go.'"
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(Tip){
   // Write your code here!
+  if(Tip==="generous"){
+    return "Thank you so much."
+  }else if(Tip=="not as generous"){
+    return "Thank you."
+  }else {
+    "Bye."
+  }
 }
